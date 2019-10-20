ec9fcaec0789bf849762204a5ad207cc6bb08018
XING YAHAO <keidarcy.1015@gmail.com>
2019-10-20 18:32:55 +0900

    update

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,8 @@ const mix = require("laravel-mix");
  |
  */
 
-mix.js(["resources/js/app.js", "resources/js/textScramble.js"], "public/js")
-    .sass("resources/sass/app.scss", "public/css")
-    .options({ processCssUrls: false });
+mix.js(
+    ["resources/js/app.js", "resources/js/textScramble.js"],
+    "public/js"
+).sass("resources/sass/app.scss", "public/css");
+// .options({ processCssUrls: false });
