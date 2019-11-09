58aea6a4252c96db70f50edeccc2ddcab1c42100
XING YAHAO <keidarcy.1015@gmail.com>
2019-11-09 14:20:47 +0900

    del original

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -1,43 +1,53 @@
 <template>
 	<v-app>
-		<v-card class="mx-auto">
+		<v-card class="mx-auto mt-12">
 			<v-img
 				class="white--text align-end"
 				height="400px"
 				src="https://images.unsplash.com/photo-1508970057347-0524a45ebdff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
 			>
-				<v-card-title class="is-capitalized">start an adventure</v-card-title>
+				<v-card-title class="is-capitalized purple--text">start an adventure</v-card-title>
 			</v-img>
 			<v-card-text class="text--primary">
 				<v-form
 					ref="form"
-					v-model="valid"
-					lazy-validation
+					@submit.prevent="login"
 				>
+					<input
+						type="hidden"
+						name="_token"
+						:value="csrf"
+					>
 					<v-text-field
-						v-model="name"
-						:counter="20"
-						:rules="nameRules"
+						v-model="email"
+						:counter="30"
 						label="Potion"
+						name="email"
 						required
+						color="purple accent-4"
+						append-icon="mdi-fountain-pen-tip"
 					></v-text-field>
 
 					<v-text-field
-						v-model="email"
-						:rules="emailRules"
+						v-model="password"
 						label="Ingredient"
+						type="password"
+						name="password"
+						color="purple accent-4"
 						required
+						append-icon="mdi-electron-framework"
 					></v-text-field>
 					<v-card-actions class="justify-center">
 						<v-btn
-							color="success"
+							color="purple accent-4"
 							class="mr-4"
-							@click="validate"
+							outlined
+							type="submit"
+							autocomplete="current-password"
 						>
 							Explore
 						</v-btn>
 					</v-card-actions>
-
 				</v-form>
 			</v-card-text>
 		</v-card>
@@ -45,5 +55,24 @@
 </template>
 
 <script>
-export default {};
+export default {
+	data() {
+		return {
+			email: '',
+			password: '',
+			csrf: document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+		};
+	},
+	methods: {
+		login() {
+			axios
+				.post('/thisisxyyo/login', {
+					email: this.email,
+					password: this.password,
+				})
+				.then(response => console.log(response.data))
+				.catch(error => console.log(error));
+		},
+	},
+};
 </script>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,43 +1,5 @@
 @extends('layouts.main')
 
 @section('content')
-<section class="section">
-    <div class="container">
-        <div class="row justify-content-center center">
-            <div class="card">
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row">
-                            <label for="email" class="center">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="input @error('email') is-invalid @enderror"
-                                    name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <label for="password" class="center">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password"
-                                    class="input @error('password') is-invalid @enderror" name="password" required
-                                    autocomplete="current-password">
-                            </div>
-                        </div>
-
-                        <div class="center">
-                            <button type="submit" class="button">
-                                {{ __('Login') }}
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
 <Login></Login>
 @endsection
